83f601b2280588c4b0a701e701ad812e553c138f
Jasmine <jmoua@csumb.edu>
2022-10-04 19:47:17 -0700

    implement feature add assignment

---
+++ src/components/AddAssignment.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Button from '@mui/material/Button';
+import Cookies from 'js-cookie';
+import { SERVER_URL } from '../constants.js';
+import TextField from '@mui/material/TextField';
+
+class AddAssignment extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { assignmentName: "", dueDate: "", courseId: "" };
+    }
+
+    handleSubmit = () => {
+        if ((this.state.assignmentName).length === 0) {
+            toast.error("Assignment added failed", {
+                position: toast.POSITION.BOTTOM_LEFT
+            });
+            return;
+        }
+        const token = Cookies.get('XSRF-TOKEN');
+        fetch(`${SERVER_URL}/assignment`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-XSRF-TOKEN': token
+                },
+                body: JSON.stringify({
+                    assignmentName: this.state.assignmentName,
+                    dueDate: this.state.dueDate,
+                    courseId: parseInt(this.state.courseId)
+                })
+            })
+            .then(res => {
+                if (res.ok) {
+                    toast.success("Assignment successfully added", {
+                        position: toast.POSITION.BOTTOM_LEFT
+                    });
+                } else {
+                    toast.error("Assignment added failed", {
+                        position: toast.POSITION.BOTTOM_LEFT
+                    });
+                    console.error('Post http status =' + res.status);
+                }
+            })
+            .catch(err => {
+                toast.error("Assignment added failed", {
+                    position: toast.POSITION.BOTTOM_LEFT
+                });
+                console.error(err);
+            });
+    }
+
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>Add New Assignment</h1>
+
+                <br /><br />
+
+                <TextField style={{ width: 200 }} label="Assignment Name" name="assignmentName"
+                    onChange={this.handleChange} value={this.state.assignmentName} />
+
+                <br /><br />
+
+                <TextField style={{ width: 200 }} label="Due Date" name="dueDate"
+                    onChange={this.handleChange} value={this.state.dueDate} InputProps={{ inputProps: {} }} />
+
+                <br /><br />
+
+                <TextField style={{ width: 200 }} label="Course Id" name="courseId"
+                    onChange={this.handleChange} value={this.state.courseId} />
+
+                <br /><br />
+
+                <Button variant="outlined" color="primary" style={{ margin: 10 }}
+                    onClick={this.handleSubmit} >Submit</Button>
+                <ToastContainer autoClose={1500} />
+            </div>
+
+        );
+    }
+}
+
+export default AddAssignment;
